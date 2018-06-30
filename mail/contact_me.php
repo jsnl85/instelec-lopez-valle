fc24ded1e7976875463641fe597dc0d764584f7a
Joao Samuel Lopes <jsnl85@gmail.com>
2018-06-30 23:26:14 +1000

    *Update: tiny updates to contacts

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -5,10 +5,10 @@ if(empty($_POST['name'])  		||
    empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
+{
 	echo "No arguments Provided!";
 	return false;
-   }
+}
 	
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'instelec@servisoft.net';
+$to = 'insteleclopezvalle@gmail.com';
 $email_subject = "[Instelec] Contacto de $name";
 $headers = "From: noreply@insteleclopezvalle.ltd\n";
 $headers .= "Reply-To: $email_address\n";
